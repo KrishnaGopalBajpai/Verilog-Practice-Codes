627d822f5d3de4d0b2f7d6bb8bf16cb83b438bf8
KrishnaGopalBajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2018-09-14 16:26:06 +0530

    32bitcounter1.v
    32 bit simple synchronous counter in behavior style.

---
+++ 32bitcounter1.v
@@ -0,0 +1,13 @@
+// 32 bit simple counter behaviour style .
+
+module sim_counter (clk,rst,count);
+input clk, rst;
+output reg [31:0] count;
+always@ (posedge clk)
+begin
+if(rst)
+count=32'b0;
+else
+count=count +1;
+end
+endmodule
