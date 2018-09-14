cceee634912881a46257ded3d8cbd087433ace8e
KrishnaGopalBajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2018-09-14 23:42:03 +0530

    Update Structuralmux2x1.v

--- Structuralmux2x1.v
+++ Structuralmux2x1.v
@@ -1,5 +1,5 @@
 //Pure structural modelling using basic gate.
-module mux2X1 (in,sel,out);
+module mux2x1m (in,sel,out);
 input [1:0]in;
 input sel;
 output out;
@@ -7,6 +7,6 @@ wire t1,t2,t3;
 
 NOT g1 (t1,sel);
 AND g2 (t2,in[0],t1);
-AND g3 (t3in[1],sel);
+AND g3 (t3,in[1],sel);
 OR g4 (out,t2,t3);
 endmodule
