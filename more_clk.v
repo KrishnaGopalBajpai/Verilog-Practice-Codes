de2fed710ccbfade9cdf56068f380826c2a977db
KrishnaGopalBajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2018-09-18 08:39:28 +0530

    Update more_clk.v
    use more then one clk in a module.
    using both the edge of the clk.

--- more_clk.v
+++ more_clk.v
@@ -1,10 +1,22 @@
-//use more then one clk.
+//use more then one clk in a module.
 
 module multi_clk (clk1,clk2,a,b,c,f1,f2);
 input clk1,clk2,a,b,c;
 output reg f1,f2;
-always@(posedge clk)
+   always@(posedge clk1)
    f1<= a&b;
-always@(negedge clk)
+   always@(negedge clk2)
    f1<= a^b;
+endmodule
+
+
+//using both the edge of the clk.
+
+module multi_use_of_bothedge_of_a_clk (a,b,f,clk);
+input clk,a,b;
+output reg f; reg t;
+   always@(posedge clk)
+    f<= t&b;
+   always@(negedge clk)
+    t<= a^b;
 endmodule
