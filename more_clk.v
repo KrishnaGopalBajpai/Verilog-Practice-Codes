83c1d659fd43dabc4d7c81f16b7b08d15fc327d0
KrishnaGopalBajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2018-09-18 08:24:44 +0530

    Update more_clk.v

--- more_clk.v
+++ more_clk.v
@@ -2,9 +2,9 @@
 
 module multi_clk (clk1,clk2,a,b,c,f1,f2);
 input clk1,clk2,a,b,c;
-output f1,f2;
+output reg f1,f2;
 always@(posedge clk)
    f1<= a&b;
-always@(posedge clk)
+always@(negedge clk)
    f1<= a^b;
 endmodule
